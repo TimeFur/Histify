1b878de14b4d973b1921fdb76ff98e81356e7921
Wayne <djs860403@gmail.com>
2023-01-11 21:55:46 +0800

    Create timeElapse effect
    1. Using requestAnimationFrame to update animation and time count down.

--- public/app/ItemLayout.js
+++ public/app/ItemLayout.js
@@ -7,15 +7,23 @@ var IL_CommProp = {
 var IL_Interface = {
     "createLayout": createSelectItemsLayout
 }
+/*********************************************
+ *              Global Resource
+ *********************************************/
+var ClassifyWrapper = null
+var FocusItem = null;
 
+var StartTimeStamp = 0
+var ElapseTime = 1800
 /*********************************************
  *                  Method
  *********************************************/
 function createSelectItemsLayout(itemsList) {
-    var classifyWrapper = document.createElement('div')
+    ClassifyWrapper = document.createElement('div')
+
+    //create child element
     var itemWrapper = document.createElement('div')
     var timeBar = document.createElement('div')
-    var focusItem = null;
 
     itemsList.forEach(itemName => {
         var item = document.createElement('div')
@@ -23,40 +31,57 @@ function createSelectItemsLayout(itemsList) {
         item.textContent = itemName
         itemWrapper.appendChild(item)
 
-        if (focusItem == null) {
-            focusItem = item;
-            focusItem.classList.add("hisify-item-focus-style")
+        if (FocusItem == null) {
+            FocusItem = item;
+            FocusItem.classList.add("hisify-item-focus-style")
         }
 
         //add listener
         item.addEventListener('click', (e) => {
-            if (focusItem != null)
-                focusItem.classList.remove("hisify-item-focus-style")
-            focusItem = item;
-            focusItem.classList.add("hisify-item-focus-style")
+            if (FocusItem != null)
+                FocusItem.classList.remove("hisify-item-focus-style")
+            FocusItem = item;
+            FocusItem.classList.add("hisify-item-focus-style")
+
+            //clear time
+            StartTimeStamp = 0;
         })
     })
     //create classify element
-    timeBar.textContent = "Time Bar"
+    timeBar.style.background = "#FF472E"
+    timeBar.style.width = "100%";
+    timeBar.style.height = "0.25rem"
+    timeBar.id = "timeBar"
 
-    classifyWrapper.appendChild(itemWrapper)
-    classifyWrapper.appendChild(timeBar)
+    ClassifyWrapper.appendChild(itemWrapper)
+    ClassifyWrapper.appendChild(timeBar)
 
+    console.log(ClassifyWrapper.querySelector("#timeBar"))
     //apend to website
     document = document.querySelector("body")
-    document.body.appendChild(classifyWrapper)
+    document.body.appendChild(ClassifyWrapper)
 
     //style
-    classifyWrapper.className = "hisify-classify-wrapper-style"
+    ClassifyWrapper.className = "hisify-classify-wrapper-style"
     itemWrapper.className = "hisify-item-wrapper-style"
 
-    //timeout setting
-    setTimeout(() => {
-        var url = window.location.href
-        var itemName = focusItem.textContent
-        classifyWrapper.style.visibility = "hidden";
-        //sendback to background [through content api]
+    window.requestAnimationFrame(step);
+}
+function step(timestamp) {
+    if (!StartTimeStamp)
+        StartTimeStamp = timestamp;
+    var progress = timestamp - StartTimeStamp;
+    var timebar = ClassifyWrapper.querySelector("#timeBar")
+    var elapseWidth = (1 - progress / ElapseTime) * 100
 
+    timebar.style.width = `${elapseWidth}%`
+    if (progress < ElapseTime) {
+        window.requestAnimationFrame(step);
+    } else {
+        var url = window.location.href
+        var itemName = FocusItem.textContent
         IL_CommProp["SEND_ITEM_CORRESPONSE_URL"]({ itemName, url })
-    }, 3000);
+
+        ClassifyWrapper.style.visibility = "hidden";
+    }
 }
