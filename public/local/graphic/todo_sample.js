fa00135d33fd69c1a62bd57bbf402a0ebc032906
Wayne <djs860403@gmail.com>
2023-01-16 22:42:52 +0800

    Create item list
    1. Dynamic add item by createItem function.
    2. todo-list-wrapper-style would wrap all item element.

--- public/local/graphic/todo_sample.js
+++ public/local/graphic/todo_sample.js
@@ -2,12 +2,17 @@
  *      Resource
  ************************/
 const QUOTE_REQUEST_URL = "https://type.fit/api/quotes"
+const ITEMLIST_TEST = [{ text: "DOC" }, { text: "WRITE ESSAY" }, { text: "YT EDIT" }]
 /************************
  *      Method
  ************************/
 const createLayout = (doc) => {
     console.log("Histofy Layout create", doc)
+
     topSection()
+    leftSection()
+    var item = createItem({ text: "Item" })
+    console.log(item)
 }
 
 const topSection = () => {
@@ -23,6 +28,42 @@ const topSection = () => {
             var index = Math.floor(Math.random() * data.length)
             quoteId.textContent = data[index].text
         });
+}
+
+const leftSection = () => {
+    //todo resource
+    const itemList = ITEMLIST_TEST
+
+    //sub-section1 - get item list 
+    var todoItemwrapper = document.querySelector('.todo-list-wrapper-style')
+    itemList.forEach(itemInfo => {
+        var itemEle = createItem(itemInfo)
+        todoItemwrapper.append(itemEle)
+    })
+
+    //sub-section2 - pie chart
+
+}
+const createItem = ({ text = "", percent = "10%" }) => {
+    var itemEle = document.createElement('div')
+    var itemIcon = document.createElement('img')
+    var textEle = document.createElement('div')
+    var arrowIcon = document.createElement('img')
+
+    itemEle.className = "item-percent-style"
+
+    itemIcon.src = "./asset/rec.png"
+
+    textEle.textContent = text
+
+    arrowIcon.src = "./asset/right-arrow.png"
+    arrowIcon.id = "arrowIcon"
+
+    itemEle.append(itemIcon)
+    itemEle.append(textEle)
+    itemEle.append(arrowIcon)
+
+    return itemEle;
 }
 /************************
  *  Prop and Interface 
